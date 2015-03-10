0e870cb14bea16b2368f4aec52dde4f91b421bad
Gattoxs <al12760321@ite.edu.mx>
2015-03-09 20:41:35 -0800

    Create BD.php
    Archivo que abrira nuestro copilador

---
+++ BD.php
@@ -0,0 +1,9 @@
+<?php
+	$var;
+	echo.  "Hola mundo esta linea no entrara porque el echo tiene un .  y no lo marcara como un imprimir ";
+	echo "AWEBO Aqui no hay error!";
+	$Variable1; = "Hello world!"; //No hay error por los ; :P!
+	int a {{{10:
+$Gatto = 7;
+	echo "$variable1";
+?>
