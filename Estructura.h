2f286cfdc55f11ca33249cd87bd099a57d901bdd
Gattoxs <al12760321@ite.edu.mx>
2015-04-26 14:36:43 -0700

    Funciones

--- Estructura.h
+++ Estructura.h
@@ -43,3 +43,74 @@ void imprimir(tipo *lista){
 		p=p->sig;
 	}
 }
+
+//_______________________Estructura de las funciones________________________________________________//
+struct mifunciones
+{	
+	char funcion[100], vars[30][100] ;
+	int renglon;
+	int VarsCant;
+	int token;
+	struct mifunciones *sig;
+	struct mifunciones *ant;
+};
+
+typedef struct mifunciones nodoF;
+typedef nodoF *tipoFuncion;
+tipoFuncion Funciones=NULL;
+int NoF = 1000;
+
+//____________________ Insertar ___________________________//
+
+void insertarFuncion(tipoFuncion *lista, int renglon, char nombre[100], char variables[30][100], int cant){
+	tipoFuncion n, p = *lista;
+	int i;
+	n=(nodoF *)malloc(sizeof(nodoF));
+	n->renglon = renglon+1;
+	n->token = NoF;
+	n->VarsCant = cant;
+	strcpy(n->funcion, nombre);
+    for(i = 0;i < cant;i++){
+    strcpy(n->vars[i], variables[i]);
+	}
+	n->sig = NULL;
+	n->ant = NULL;
+	NoF++;
+	
+		if(*lista == NULL)
+		*lista = n;
+	else
+		{
+			while(p->sig != NULL)
+				p=p->sig;
+				p->sig = n;
+				n->ant = p;
+		}		
+	
+}
+
+//______________________ Imprimir Funciones___________________________//
+void imprimirFunciones(tipoFuncion *lista){
+	tipoFuncion p = *lista;
+	int i;
+	while(p != NULL){
+		printf("Nombre [%s]\nCantidad de var [%d]\nVariables [", p->funcion, p->VarsCant);
+		
+		for(i = 0; i < p->VarsCant ;i++)
+		printf("%s, ", p->vars[i]);
+		
+		printf("]\nToken: [%d]\n", p->token);
+		p=p->sig;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
