0c67c0176a68370c50927dcdc347c213eab50097
Gattoxs <al12760321@ite.edu.mx>
2015-05-31 19:42:31 -0700

    Codigo usado para verificar el copilador

---
+++ source.c
@@ -0,0 +1,11 @@
+main (){
+	printf("Hello! This is our final copilator of the class Automatas!");
+	int a = 5, x, r = 5;
+	float uno = 565.32, dos = 5685.2;
+	char cada2 = "HELLO WORLDS", ca1, cadena = "que tal";
+	b = a + uno;
+	float b;
+	cadena = "Cambio";
+	a = cadena;
+
+}
