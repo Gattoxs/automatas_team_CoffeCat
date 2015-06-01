f7620082b63a917d0ee49ce75675007e00a37503
Gattoxs <al12760321@ite.edu.mx>
2015-05-31 19:31:51 -0700

    Guarda las variables en una lista propia

--- Ordenar.h
+++ Ordenar.h
@@ -8,13 +8,14 @@
      strcat(str, a);
      return str;
  }
+ 
 //______________________ Guardar ____________________//
 bool ex=true;
-void guardar(tipo *lista, FILE *Archivo, char res[118][20]){
+
+void guardar(tipo *lista, FILE *Archivo){
 	tipo n, p=*lista;
-	char pala[100], a[100], z, vars[30][100];
-	int x,y, i=0, s=0;
-	
+	char pala[100], a[100], z, vars[30][100], name[100], value[100];
+	int x,y, i=0, s=0, type=0, dot=0, eq=0;
 	
 //printf("Archivo original: \n");	
 //	while(!feof(Archivo)){
@@ -24,13 +25,14 @@ void guardar(tipo *lista, FILE *Archivo, char res[118][20]){
 rewind(Archivo);	
 	x = fgetc(Archivo);
 	pala[0] = 0;
+	
 	if(lista){
-		while(!feof(Archivo))
-		{ 
-		back:
+		while(!feof(Archivo)){ 
+		
+			back:
 			
-		//function
-		if(AToken(pala) == 335){
+			//function
+			if(AToken(pala) == 335){
 			insertar(&*lista, i, pala, AToken(pala));
 			pala[0] = 0;
 			for(s = 0; s<30; s++) vars[s][0]='\0';
@@ -105,19 +107,32 @@ rewind(Archivo);
 					goto END;
 				}				
 		}
-		
-		
-		//espacio
-		if(x == 32){
-			if(strlen(pala) > 1)
-			insertar(&*lista, i, pala, AToken(pala));
-			x = fgetc(Archivo);
-			pala[0] = 0;
+			// MATH SENTENCES
+			// < = > ! & % * + - / ^ [ ] { } | , . ;
+			if( x == 60 || x == 61 || x == 62|| x == 33 || x == 37 || x == 38 || x == 42 || x == 43 || x == 45 || x == 47 || x == 94 || x == 91 || x == 93 || x == 123 || x == 125 || x == 124 || x == 44 || x == 46 || x == 59 ){
+				if(strlen(pala))
+					insertar(&*lista, i, pala, AToken(pala));
+				pala[0] = 0;
+				strcatint(pala, x);
+				insertar(&*lista, i, pala, AToken(pala));
+				pala[0] = 0;
+				x = fgetc(Archivo);
+				goto back;
+			}
+			
+			//espacio
+			if(x == 32){
+				
+				if(strlen(pala) >= 1)
+					insertar(&*lista, i, pala, AToken(pala));
+				x = fgetc(Archivo);
+				pala[0] = 0;
+				
 			goto back;
 		}	
-				
-		//Salto de linea
-		if(x == 10){
+					
+			//Salto de linea
+			if(x == 10){
 			if(strlen(pala) > 1)
 			insertar(&*lista, i, pala, AToken(pala));
 			i = i + 1;
@@ -125,31 +140,32 @@ rewind(Archivo);
 			pala[0] = 0;
 			goto back;
 		}			
-		//Tabulacion
-		if(x == 9){
 			
-			if(strlen(pala) > 1)
-			insertar(&*lista, i, pala, AToken(pala));
-			x = fgetc(Archivo);
-			pala[0] = 0;
-			goto back;
-		}		
-		
-		//Parentesis
-		if(x == 40 || x == 41){
-		if(pala[0] != 0){
-			insertar(&*lista, i, pala, AToken(pala));
-		}
-		pala[0] = 0;
-		strcatint(pala, x);
-		insertar(&*lista, i, pala, AToken(pala));
-		pala[0] = 0;
-		x = fgetc(Archivo);
-		goto back;
-		}
-		
-		//Numeros
-		if(x >= 48 && x <= 57){
+			//Tabulacion
+			if(x == 9){
+			
+				if(strlen(pala) > 1)
+					insertar(&*lista, i, pala, AToken(pala));
+					x = fgetc(Archivo);
+					pala[0] = 0;
+					goto back;
+			}		
+			
+			//Parentesis
+			if(x == 40 || x == 41){
+				if(pala[0] != 0){
+					insertar(&*lista, i, pala, AToken(pala));
+				}
+				pala[0] = 0;
+				strcatint(pala, x);
+				insertar(&*lista, i, pala, AToken(pala));
+				pala[0] = 0;
+				x = fgetc(Archivo);
+				goto back;
+			}
+			
+			//Numeros
+			if(x >= 48 && x <= 57){
 			if(strlen(pala) > 1)
 			insertar(&*lista, i, pala, AToken(pala));
 			pala[0] = 0;
@@ -162,12 +178,13 @@ rewind(Archivo);
 				x = fgetc(Archivo);	
 			}
 			
-			insertar(&*lista, i, pala, 306);
+			insertar(&*lista, i, pala, 89);
 			pala[0] = 0;
 			goto back;
 		}		
-		//Comillas
-		if(x == 34){
+			
+			//Comillas
+			if(x == 34){
 			pala[0] = 34;
 			insertar(&*lista, i, pala, 34);
 			pala[0] = 0;
@@ -177,7 +194,7 @@ rewind(Archivo);
 				strcatint(pala, x);
 				x = fgetc(Archivo);
 			}
-			insertar(&*lista, i, pala, 308);
+			insertar(&*lista, i, pala, 88);
 			pala[0] = 0;
 			
 			strcatint(pala, x);
@@ -187,56 +204,273 @@ rewind(Archivo);
 			goto back;
 		}
 		
-		
-		//Variable
-		if(x == 36){
-			strcatint(pala, x);
-			y = 0;
-			insertar(&*lista, i, pala, 309);
-			pala[0] = 0;
-			x = fgetc(Archivo);
-			z = x;
-			a[0] = z;
-			a[1] = 0;
-			if(x == 97 | x == 98 | x == 99 | x == 100 | x == 101 | x == 102 | x == 103 | x == 104 | x == 105 | x == 106 | x == 107 | x == 108 | x == 109 | x == 110 | x == 111 | x == 112 | x == 113 | x == 114 | x == 115 | x == 116 | x == 117 | x == 118 | x == 119 | x == 120 | x == 121 | x == 122 | x == 65 |x == 66 |x == 67 |x == 68 |x == 69 |x == 70 |x == 71 |x == 72 |x == 73 |x == 74 |x == 75 |x == 76 |x == 77 |x == 78 |x == 79 |x == 80 |x == 81 |x == 82 |x == 83 |x == 84 |x == 85 |x == 86 |x == 87 |x == 89 |x == 90)
-			{
-			while(x != 9 && x != 32 && x != 61 && x != 45 && x != 43 && x != 47 && x != 34 && x != 59  && x != 58  && x != 123  && x != 125  && x != 91  && x != 94  && x != 40){
+			// Plabras reservadas	
+			if(x != 10 && x != 32 && x != 34 && x != 9 && x != 40  && x != 41 ){
+							
 				strcatint(pala, x);
-				x = fgetc(Archivo);
-			}	
-			insertar(&*lista, i, pala, 310);
-			pala[0] = 0;
+				y = 0;
+			
+			//Variable
+			if(AToken(pala) == 51 || AToken(pala) == 52 || AToken(pala) == 53 || AToken(pala) == 54 ){
+			
+			//						          Char                       INT                       FLOAT                     DOUBLE
+			switch(AToken(pala)){  case 51: type=51; break;   case 52: type=52; break;   case 53: type=53; break;  case 54: type=54; break;} 
+			x = fgetc(Archivo);
+			if(x == 32){
+				insertar(&*lista, i, pala, AToken(pala));
+				pala[0] = 0;
+				
+				variablename:
+					name[0] = 0;
+				//Removing spaces
+				while(x == 32)
+					x = fgetc(Archivo);
+					
+				//Name of variable(s)
+				
+				if(x >= 48 && x <= 57){
+					ex=false;
+						printf("Sorry but variable names cant start with numbers.");
+						goto END;
+				}
+				
+				//           SPACE    =          ,          [         ;
+				while( x != 32 && x != 61 && x != 44 && x != 91 && x != 59){
+					
+					
+					if(x >= 65 && x <=90 || x >= 97 && x <=122 || x >= 48 && x <= 57){ //This condition indicates that the variables' name are only letters
+					
+						strcatint(pala, x);
+						x = fgetc(Archivo);	
+					}else{
+						if(x != 32 || x == 61 || x != 44 || x != 59){//If the condition is false, the varible 'ex' indicates that an error was found in the code
+						ex=false;
+						printf("Sorry but you can't asign special caracters to variable name.");
+						goto END;
+						}
+					}
+				}
+				
+				//Insert the variable name to the list with the Token #87
+				insertar(&*lista, i, pala, 87);	
+				strcpy(name, pala);				
+				pala[0] = 0;
+				
+				//After variable name
+				aftervariablename:
+				switch(x){
+					
+					//Removing spaces
+					case 32:
+						while(x == 32)
+							x = fgetc(Archivo);
+						goto aftervariablename;	
+					break;	
+					
+					//Equal
+					case 61:
+						eq=1;
+						strcatint(pala, x);
+						insertar(&*lista, i, pala, AToken(pala));	
+						x = fgetc(Archivo);
+						pala[0] = 0;
+						//Removing spaces
+						while(x == 32)
+							x = fgetc(Archivo);
+							
+						switch(x){
+							
+							case 34:
+								//If the variable is char can asign strings 
+								if(type == 51){
+									strcatint(pala, x);
+									insertar(&*lista, i, pala, AToken(pala));	
+									x = fgetc(Archivo);
+									pala[0] = 0;
+									//Getting whole string
+									while(x != 34){
+										strcatint(pala, x);
+										x=fgetc(Archivo);
+									}
+								
+									//After String
+									insertar(&*lista, i, pala, 88);	
+									insertarType(&types, i, name, 89, pala);
+									pala[0] = 0;
+									strcatint(pala, x);
+									insertar(&*lista, i, pala, AToken(pala));	
+									x = fgetc(Archivo);
+									pala[0] = 0;
+									goto aftervariablename;
+									
+								//Else will end the code with the error mark
+								}else{
+									ex=false;
+									printf("Its not char type.");
+									goto END;
+								}
+								
+							break;
+							
+							case 48: // 0
+							case 49: // 1
+							case 50: // 2
+							case 51: // 3
+							case 52: // 4
+							case 53: // 5
+							case 54: // 6
+							case 55: // 7
+							case 56: // 8
+							case 57: // 9
+							case 46: // .
+								if(type == 52 || type == 53 || type == 54){
+									
+									//While x get the value of , or ;
+									while(x != 44 && x != 59){
+									
+										//If the x value is number or dot, continue, else error
+										if(x >= 48 && x <= 57 || x == 46){
+											
+											//Concat the value
+											strcatint(pala,x);
+											x = fgetc(Archivo);
+											
+											//If the value have decimal
+											if(x == 46){
+												
+												//Increase the dot count
+												dot++;
+												//If the value of dot is more than 1 or the type of the variable is INT make the error
+												if(dot > 1 || type == 52){
+													ex=false;
+													printf("So many dots in the value.");
+													goto END;
+												}
+											}
+											
+										}else{
+											ex=false;
+											printf("Error");
+											goto END;
+										}									
+									}
+										
+									//After getting the whole value
+									
+									insertar(&*lista, i, pala, 89);	
+									strcpy(value, pala);
+									
+									if(type == 52) //INT
+										insertarType(&types, i, name, 90, value);
+									
+									if(type == 53) //FLOAT
+										insertarType(&types, i, name, 91, value);
+									
+									if(type == 54) //double
+										insertarType(&types, i, name, 92, value);
+									
+									pala[0] = 0;
+									dot = 0;
+									goto aftervariablename;
+									
+									
+									
+								}else{
+									ex=false;
+									printf("Error");
+									goto END;
+								}
+								
+							break;
+						}
+						
+					break;	
+					
+					//Coma
+					case 44:
+						if(!eq){
+								
+							if(type==51){
+								insertarType(&types, i, name, 89, "");
+						
+							}else{
+								if(type == 52) //INT
+									insertarType(&types, i, name, 90, "0");
+								
+								if(type == 53) //FLOAT
+									insertarType(&types, i, name, 91, "0");
+								
+								if(type == 54) //double
+									insertarType(&types, i, name, 92, "0");
+							}
+						}
+						
+						eq=0;
+						strcatint(pala, x);
+						insertar(&*lista, i, pala, AToken(pala));	
+						x = fgetc(Archivo);
+						pala[0] = 0;
+						goto variablename;
+						
+					break;		
+						
+					//DotComa					
+					case 59:
+						if(!eq){
+								
+							if(type==51){
+								insertarType(&types, i, name, 89, "");
+						
+							}else{
+								
+								if(type == 52) //INT
+									insertarType(&types, i, name, 90, "0");
+								
+								if(type == 53) //FLOAT
+									insertarType(&types, i, name, 91, "0");
+								
+								if(type == 54) //double
+									insertarType(&types, i, name, 92, "0");
+							}
+						}
+						
+						eq=0;
+						strcatint(pala, x);
+						insertar(&*lista, i, pala, AToken(pala));	
+						pala[0] = 0;
+						goto next;
+						
+					break;	
+
+					default:
+						ex=false;
+						printf("\nSyntax error!\n On line [%d] near to [%s]\n", i+1, pala);
+						goto END;
+						break;
+						
+				}
+			}
+		}				
+				while(y < 80){
+					if(strcmp(atoken[y].carc, pala) == 0){
+						if(AToken(pala) == 51 || AToken(pala) == 52 || AToken(pala) == 53 || AToken(pala) == 54 ){}
+					
+						insertar(&*lista, i, pala, AToken(pala));
+						pala[0] = 0;
+		
+					}
+					y++;
+				}
+					
+	
 			}
 			
-			goto back;
-		}
-		
-		// Plabras reservadas	
-		
-		if(x != 10 && x != 32 && x != 34 && x != 36 && x != 9 ){
-		//printf("Pala: %s\nX: %c\n",pala, x);
-		//system("pause");	
-			strcatint(pala, x);
-			y = 0;
+				next:
+			x = fgetc(Archivo);			
 			
-		while(y < 27){
-				if(strcmp(pala, res[y]) == 0){
-					insertar(&*lista, i, pala, AToken(pala));
-					pala[0] = 0;
-					goto next;
-				}
-
-			y = y + 1;
-			
-		}
-
-		next:
-		x = fgetc(Archivo);
-		
-		}	
-
    		}
    	END:
-   	 printf("");	
+   	 printf("");
+			
 	}
 }
